7ee32fb396183a2a2e99ce3d9c81f8d6c1f1a579
Umar Farooq <ufservices465@gmail.com>
2025-01-27 19:57:51 +0530

    hero section modified

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,14 @@ import { FastForward, Shield, ShieldIcon, Upload } from "lucide-react";
 import { MagicCard } from "@/components/ui/magic-card";
 import { useTheme } from "next-themes";
 import { Splash } from "next/font/google";
+import { Hero } from "@/components/hero";
 
 function HeroSection() {
   const { theme } = useTheme();
 
   return (
     <main className="min-h-screen">
-      <Spotlight className="top-10 left-0 md:left-60 md:-top-20" fill="white" />
+      {/* <Spotlight className="top-10 left-0 md:left-60 md:-top-20" fill="white" />
       <div className=" relative h-full  w-full text-center">
         <div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col gap-4 items-center justify-center">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
@@ -67,7 +68,8 @@ function HeroSection() {
             </MagicCard>
           </div>
         </section>
-      </div>
+      </div> */}
+      <Hero />
     </main>
   );
 }
